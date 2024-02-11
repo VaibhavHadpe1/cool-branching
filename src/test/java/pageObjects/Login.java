197309ff09982d3d4abfa464b23836236975db60
vaibhav <hadpevaibhav777@gmail.com>
2024-02-11 19:10:30 +0530

    Removed Spaces between the lines

--- src/test/java/pageObjects/Login.java
+++ src/test/java/pageObjects/Login.java
@@ -5,16 +5,10 @@ public class Login
     public static void main(String[] args)
     {
         System.out.println("Login Page Object");
-
         System.out.println("Login Page Object added line from Dev branch and want conflicts.No.No");
-
         System.out.println();
         System.out.println("..........................");
-
-
-
         System.out.println("I want conflicts");
-
         System.out.println("Login Page Object added line from Dev branch and want conflicts.Conflicts.Conflicts.");
 
 
