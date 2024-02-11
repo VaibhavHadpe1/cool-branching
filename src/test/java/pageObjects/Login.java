b816f27adc2e84fe9f8b4a7e2075424730c90144
vaibhav <hadpevaibhav777@gmail.com>
2024-02-11 18:48:01 +0530

    Changed same line

--- src/test/java/pageObjects/Login.java
+++ src/test/java/pageObjects/Login.java
@@ -5,6 +5,6 @@ public class Login
     public static void main(String[] args)
     {
         System.out.println("Login Page Object");
-        System.out.println("Login Page Object added line from Dev branch and want conflicts.");
+        System.out.println("Login Page Object added line from Dev branch and want conflicts.No.No");
     }
 }
