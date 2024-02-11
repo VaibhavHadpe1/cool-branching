78d56bbbc6e48c1af84d9d7c08c86b15bd3b2804
vaibhav <hadpevaibhav777@gmail.com>
2024-02-11 18:38:48 +0530

    Changed second line in login class for conflict

--- src/test/java/pageObjects/Login.java
+++ src/test/java/pageObjects/Login.java
@@ -5,6 +5,6 @@ public class Login
     public static void main(String[] args)
     {
         System.out.println("Login Page Object");
-        System.out.println("Login Page Object added line from Dev branch.");
+        System.out.println("Login Page Object added line from Dev branch and want conflicts.");
     }
 }
